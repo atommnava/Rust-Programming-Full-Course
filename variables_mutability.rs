ede1fb89631b1f98d57904389d0dbf641107948c
atommnava <atommunoznava@gmail.com>
2025-06-01 12:53:13 -0600

    Vars and mutability

---
+++ variables_mutability.rs
@@ -0,0 +1,10 @@
+fn main()
+{
+    println!("");
+    // By default a variable is INMUTABLE 
+    // it means that it cannot change
+    let mut a: u16 = 5;
+    println!("{}", a);
+    a = 10;
+    println!("{}", a);
+}
