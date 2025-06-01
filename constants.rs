9bc3737854c84753d0370fc931a1470e489b1f2f
atommnava <atommunoznava@gmail.com>
2025-06-01 13:01:38 -0600

    Constants

---
+++ constants.rs
@@ -0,0 +1,12 @@
+// Constants
+fn main()
+{
+    let mut x: i32 = 2;
+    const Y: i32 = 10;
+    println!("{}", x);
+    println!("{}", Y);
+    println!("{:.2}", PI);
+}
+
+// U can declare a constant here WITH TYPE ANNOTATION
+const PI: f64 = 3.13159342;
